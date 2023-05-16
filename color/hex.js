5e52208fd4ff07e936ec06b93ac90cda46761f3b
ayanami <3139441442@qq.com>
2023-05-16 08:54:54 +0800

    refactor：删除HexToR判断是否透明的入参

--- color/hex.js
+++ color/hex.js
@@ -1,16 +1,15 @@
 import { HexToRgb } from "../utils/fn.js";
 /**
  * @param str  string 6位Hex
- * @param boolOp boolean 是否rgba
  * @param strOp string 透明度
  * @returns string rgba||rgb
  */
-const HexToR = (str, boolOp, strOp = 1) => {
+const HexToR = (str, strOp = 1) => {
     const hex = str.split("#")[1];
     console.log(hex, "33");
     if (hex.length === 6) {
         let strColor = HexToRgb(hex);
-        const strFinal = `${strColor()},${strColor()},${strColor()}${boolOp ? "," + strOp : ""}`;
+        const strFinal = `${strColor()},${strColor()},${strColor()}${strOp ? "," + strOp : ""}`;
         strColor = null;
         return strFinal;
     } else {
